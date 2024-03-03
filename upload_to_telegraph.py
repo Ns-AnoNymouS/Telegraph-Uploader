403c76f4c34756983fcb44d2f7623d778dba330b
Bhadvaheroku <156423343+Bhadvaheroku@users.noreply.github.com>
2024-03-03 18:58:44 +0530

    Update upload_to_telegraph.py

--- upload_to_telegraph.py
+++ upload_to_telegraph.py
@@ -7,28 +7,23 @@ logging.getLogger().setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 logger = logging.getLogger(__name__)
 
-import os
 import time
 from config import Config
-from helpers import progress
-from telegraph import upload_file, Telegraph
+from telegraph import Telegraph
 from pyrogram import Client, filters
 
-
 telegraphbot = Client("TELEGRAPH",
-                 bot_token=Config.BOT_TOKEN,
-                 api_id=Config.API_ID,
-                 api_hash=Config.API_HASH,
-                 parse_mode="markdown",
-                 workers=100)
-
+                      bot_token=Config.BOT_TOKEN,
+                      api_id=Config.API_ID,
+                      api_hash=Config.API_HASH,
+                      parse_mode="markdown",
+                      workers=100)
 
 @telegraphbot.on_message(filters.command('start') & filters.incoming)
 async def start_handlers(c, m):
     await m.reply_text(
         "Hello **Dear!**\n\n"
         "I am a telegra.ph uploader.\n\n"
-        "⍟ I can upload photos to telegra.ph and gives you the link.\n"
         "⍟ I can create a instant view link for your text.\n"
         "⍟ I can create post in telegra.ph if you send any text.\n"
         "(You can send text in format `post content|TITLE`)\n\n"
@@ -36,37 +31,6 @@ async def start_handlers(c, m):
         disable_web_page_preview=True,
         quote=True
     )
-
-
-@telegraphbot.on_message(filters.photo & filters.incoming)
-async def telegraph(c, m):
-    """Uploading to photo to telegra.ph 
-       and sending photo link to user"""
-
-    try:
-        send_message = await m.reply_text(
-            "Processing....⏳", 
-            quote=True
-        )
-        location = f'./{m.from_user.id}{time.time()}/'
-        start_time = time.time()
-        file = await m.download(
-            location,
-            progress=progress,
-            progress_args=(send_message, start_time)
-        )
-        try:
-            media_upload = upload_file(file)
-        except Exception as e:
-            print('An Error occurred', e)
-            await send_message.edit(f'**Error:**\n{e}')
-        telegraph_link = f'https://telegra.ph{media_upload[0]}'
-        await send_message.edit(
-            telegraph_link
-        )
-    except:
-        pass
-
 
 @telegraphbot.on_message(filters.text & filters.incoming)
 async def text_handler(c, m):
@@ -96,8 +60,8 @@ async def text_handler(c, m):
             print(e)
         await m.reply_text("https://telegra.ph/{}".format(response["path"]))
 
-    except:
-        pass
-
+    except Exception as e:
+        logger.error(f"Error in text_handler: {e}")
 
 telegraphbot.run()
+graphbot.run()
